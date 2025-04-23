9ee09bff6413b3d5a9553df52281a8b22bc36c49
FixxxarN <tornstromandre@gmail.com>
2025-04-23 22:52:29 +0200

    Setup electron and react

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Layout from './components/Layout/index.jsx';
+
+const root = createRoot(document.body);
+root.render(<Layout />);
